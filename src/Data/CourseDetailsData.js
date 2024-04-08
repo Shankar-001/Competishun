169da599100d75fcd6a0e683b4bd55c691956e5f
Shankar-001 <praveen01.ugec20@iiitranchi.ac.in>
2024-04-08 16:31:33 +0530

    courseDetails css

--- src/Data/CourseDetailsData.js
+++ src/Data/CourseDetailsData.js
@@ -13,7 +13,7 @@ export const SingleCourseDetails = {
     ],
     RefundPolicy: 'https://bit.ly/49LLzYy',
   },
-  
+
   PRAKHAR: {
     Title: 'PRAKHAR (XII) – Target : IIT- JEE (MAIN+ADVANCED) – 2025',
     AboutCourse:
@@ -168,6 +168,13 @@ export const CommonCourseData = {
     'Noble Intent, Genuine Advice, Social Work: Benefit from noble intentions, genuine advice, and social welfare commitment.',
   ],
 
-  choose: 'Choose Competishun - Where Excellence Meets Innovation!',
-  contact: 'For more queries, Contact us at: 8888000021, 7410900901',
+  Stages: 'https://www.youtube.com/embed/iR5ZCto7Td4',
+  OnlineVsOffline: 'https://www.youtube.com/embed/JKg9Ked2aIk',
+  KotaVsJaipurVsHome: 'https://www.youtube.com/embed/qjr3_qtdWFg',
+  GeneralQNA: 'https://www.youtube.com/embed/50hiqJY_8h0',
+  CourseFeatureVideo: 'https://www.youtube.com/embed/l2tLkv_t15k',
+  AdvantagesVideo: 'https://www.youtube.com/embed/06dJv3gJX88',
+
+  Choose: 'Choose Competishun - Where Excellence Meets Innovation!',
+  Contact: 'For more queries, Contact us at: 8888000021, 7410900901',
 };
