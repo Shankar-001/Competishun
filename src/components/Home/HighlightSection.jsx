b4b930cf3a086ced974b656da7a7b0f20a3d670c
jigovind556 <jigovind556@gmail.com>
2024-04-09 13:53:07 +0530

    hosted highlight video on firebase

--- src/components/Home/HighlightSection.jsx
+++ src/components/Home/HighlightSection.jsx
@@ -4,7 +4,11 @@ const HighlightSection = () => {
   return (
     <div className="highlight">
       <video autoPlay loop muted>
-        <source src="src/assets/highlight-background.mp4" type="video/mp4" />
+        {/* <source src="src/assets/highlight-background.mp4" type="video/mp4" /> */}
+        <source
+          src="https://firebasestorage.googleapis.com/v0/b/couplematch-47708.appspot.com/o/videos%2Ftemp%2Fhighlight-background.mp4?alt=media&token=13772d22-7a7a-4ed9-8cb6-40b184f7e125"
+          type="video/mp4"
+        />
         {/* Your browser does not support the video tag. */}
       </video>
       <div className="overlay">
