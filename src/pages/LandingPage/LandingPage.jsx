867551ec1072d636c38d6a6bbe2d7fadb48f68d2
abhi kumar <9984907494abhi@gmail.com>
2024-04-16 18:44:55 +0530

    result section and courses section

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,15 @@
+// import CoursesSection from "../../components/LandingComponent/CoursesSection";
+
+import ResultSection from "../../components/LandingComponent/ResultSection";
+
+// import { ResultSection } from "../../components/LandingComponent/ResultSection";
 const LandingPage = () => {
   return (
-    <div className="Landing-Page-Main-Container">LandingPage</div>
-  )
-}
-export default LandingPage
+    <div className="Landing-Page-Main-Container">
+   {/* <CoursesSection />
+    */}
+    <ResultSection />
+      </div>
+  );
+};
+export default LandingPage;
