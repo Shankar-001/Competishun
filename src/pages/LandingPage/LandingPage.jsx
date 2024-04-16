427670e857804d56035f1608ebc23a3b7a3924f7
Shankar-001 <praveen01.ugec20@iiitranchi.ac.in>
2024-04-16 18:34:04 +0530

    landing FAQ and HomeSection

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,12 @@
+import LandingFAQ from "../../components/LandingComponent/LandingFAQ"
+import LandingHomeSection from "../../components/LandingComponent/LandingHomeSection"
+
 const LandingPage = () => {
   return (
-    <div className="Landing-Page-Main-Container">LandingPage</div>
+    <div className="Landing-Page-Main-Container">
+      <LandingHomeSection />
+      <LandingFAQ />
+    </div>
   )
 }
 export default LandingPage
