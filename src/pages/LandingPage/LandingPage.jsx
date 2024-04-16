ef89c9614a0b740d50096404474cf188805b83e9
jigovind556 <jigovind556@gmail.com>
2024-04-16 13:19:27 +0530

    completed the testimonial section of landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,10 @@
+import LandingTestimonials from "../../components/LandingComponent/LandingTestimonials"
+
 const LandingPage = () => {
   return (
-    <div className="Landing-Page-Main-Container">LandingPage</div>
+    <div className="Landing-Page-Main-Container">
+      <LandingTestimonials/>  
+    </div>
   )
 }
 export default LandingPage
