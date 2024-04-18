d1c197e10a75c655509e3a97345d2a03f0fd470d
Anshu470456 <ashu470456@gmail.com>
2024-04-18 16:14:24 +0530

    added gif to importantLInkSection

--- src/components/LandingComponent/LandingWhyCompethisun.jsx
+++ src/components/LandingComponent/LandingWhyCompethisun.jsx
@@ -1,69 +1,117 @@
 import React from 'react';
-import { LuCheckCircle } from 'react-icons/lu';
-import student from '../../assets/student.png';
-
+import { GiFlowerStar } from 'react-icons/gi';
+import gif from '../../assets/click2.gif';
+import { GiClick } from 'react-icons/gi';
 export const LandingWhyCompethisun = () => {
+  
   return (
     <div className="main-container">
       <div className="inner-container">
         <div className="content">
           <h1 className="heading-whyCompetishun">
-            Why <span>Join</span> Competishun?{' '}
+            Important Links
           </h1>
 
           <div className="box1" id="abc">
             <div className="check-logo">
-              <LuCheckCircle color="#25D366" className='check' />
+              {/* <LuCheckCircle color="orage" className='check' /> */}
+              <GiFlowerStar color="orange" className="check" />
             </div>
             <div className="info">
-              <p>Seasoned and experienced subject experts</p>
+              <a
+                href="https://youtu.be/l2tLkv_t15k?feature=shared"
+                target="_blank"
+              >
+                <p>Course Features</p>
+              </a>
+            </div>
+            {/* <div className="info">
+              <a
+                href="https://youtu.be/l2tLkv_t15k?feature=shared"
+                target="_blank"
+              >
+                <GiClick color="orange" className="check" />
+              </a>
+            </div> */}
+          </div>
+
+          <div className="box1">
+            <div className="check-logo">
+              <GiFlowerStar color="orange" className="check" />
+            </div>
+            <div className="info">
+              <a
+                href="https://youtu.be/06dJv3gJX88?feature=shared"
+                target="_blank"
+              >
+                <p>Why Join Us? Competishun Advantage!</p>
+              </a>
             </div>
           </div>
 
           <div className="box1">
             <div className="check-logo">
-              <LuCheckCircle color="#25D366"  className='check'/>
+              <GiFlowerStar color="orange" className="check" />
             </div>
             <div className="info">
-              <p>20 years of experience in education sector.</p>
+              <a
+                href="https://youtu.be/iR5ZCto7Td4?feature=shared"
+                target="_blank"
+              >
+                <p>Preparation Stages</p>
+              </a>
             </div>
           </div>
 
           <div className="box1">
             <div className="check-logo">
-              <LuCheckCircle color="#25D366"  className='check'/>
+              <GiFlowerStar color="orange" className="check" />
             </div>
             <div className="info">
-              <p>Enriched content which makes your path easier.</p>
+              <a
+                href="https://youtu.be/50hiqJY_8h0?feature=shared"
+                target="_blank"
+              >
+                <p>General Question</p>
+              </a>
             </div>
           </div>
 
           <div className="box1">
             <div className="check-logo">
-              <LuCheckCircle color="#25D366" className='check' />
+              <GiFlowerStar color="orange" className="check" />
             </div>
             <div className="info">
-              <p>Thousands of success stories</p>
+              <a
+                href="https://youtu.be/JKg9Ked2aIk?feature=shared"
+                target="_blank"
+              >
+                <p>Online Vs Offline</p>
+              </a>
             </div>
           </div>
 
           <div className="box1">
             <div className="check-logo">
-              <LuCheckCircle color="#25D366" className='check' />
+              <GiFlowerStar color="orange" className="check" />
             </div>
             <div className="info">
-              <p>Optimum usage of technology</p>
+              <a
+                href="https://youtu.be/qjr3_qtdWFg?feature=shared"
+                target="_blank"
+              >
+                <p>Kota vs Jaipur vs Home</p>
+              </a>
             </div>
           </div>
         </div>
 
         <div className="image">
-          <img src={student} alt="student" className="student-image" />
+          <img src={gif} alt="student" className="student-image" />
+          {/* <video src={video} alt="student" className="student-image" autoPlay poster={pic}/> */}
         </div>
       </div>
-      <button className="btn">
-        Join Now
-      </button>
+      <button className="btn">Join Now</button>
     </div>
   );
 };
