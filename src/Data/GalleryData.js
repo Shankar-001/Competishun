c4fa99d9a6e4c75158d4e8b0ab3f9bf9f10166ec
Anshu470456 <ashu470456@gmail.com>
2024-04-20 19:06:42 +0530

    fixed a bug in footer section

--- src/Data/GalleryData.js
+++ src/Data/GalleryData.js
@@ -4,44 +4,27 @@ import achi2 from '../assets/Gallery/Achievements/shark tankkk.jpg';
 import achi3 from '../assets/Gallery/Achievements/dubai.jpg';
 import achi4 from '../assets/Gallery/Achievements/award.jpg';
 //  celebration ------------------------------------------------------------------------------
-import celeb1 from '../assets/Gallery/Celebration/celebration1.jpg';
-import celeb2 from '../assets/Gallery/Celebration/celebration2.jpg';
-import celeb3 from '../assets/Gallery/Celebration/celebration3.jpg';
-import celeb4 from '../assets/Gallery/Celebration/celebration4.jpg';
-import celeb5 from '../assets/Gallery/Celebration/celebration5.jpg';
-import celeb6 from '../assets/Gallery/Celebration/celebration6.jpg';
+
 import celeb7 from '../assets/Gallery/Celebration/celebration7.jpg';
-import celeb8 from '../assets/Gallery/Celebration/celebration8.jpg';
-import celeb9 from '../assets/Gallery/Celebration/celebration9.jpg';
-import celeb10 from '../assets/Gallery/Celebration/celebration10.jpg';
 import celeb11 from '../assets/Gallery/Celebration/celebration11.jpg';
-import celeb12 from '../assets/Gallery/Celebration/celebration12.jpg';
 import celeb13 from '../assets/Gallery/Celebration/celebration13.jpg';
-import celeb14 from '../assets/Gallery/Celebration/celebration14.jpg';
-import celeb15 from '../assets/Gallery/Celebration/celebration15.jpg';
-import celeb16 from '../assets/Gallery/Celebration/celebration16.jpg';
 import celeb17 from '../assets/Gallery/Celebration/celebration17.jpg';
-import celeb18 from '../assets/Gallery/Celebration/celebration18.jpg';
-import celeb19 from '../assets/Gallery/Celebration/celebration19.jpg';
-import celeb20 from '../assets/Gallery/Celebration/celebration20.jpg';
-import celeb21 from '../assets/Gallery/Celebration/celebration21.jpg';
-import celeb22 from '../assets/Gallery/Celebration/celebration22.jpg';
 import celeb23 from '../assets/Gallery/Celebration/celebration23.jpg';
 
 
 
 export const GalleryDataAchiev = [
-  { src: achi1 , desc: 'Received Youtube Golden Play Button (1 M Subscribers). Content For IIT-JEE is available for FREE at /Mohit Tyagi Youtube Channel.' },
-  { src: achi2 , desc: 'Featured in Shark Tank India Season-3 Ep-49' },
-  { src: achi3 , desc: 'Awarded with South East Asia Business Icon in Dubai in an event organized by Dainik Bhaskar' },
-  { src: achi4 , desc: 'Awarded with excellence in Social Welfare in Education by Education Minister (Rajasthan) Shri B.D. Kala  ' },
+  { src: achi1, desc: 'Received Youtube Golden Play Button (1 M Subscribers). Content For IIT-JEE is available for FREE at /Mohit Tyagi Youtube Channel.' },
+  { src: achi2, desc: 'Featured in Shark Tank India Season-3 Ep-49' },
+  { src: achi3, desc: 'Awarded with South East Asia Business Icon in Dubai in an event organized by Dainik Bhaskar' },
+  { src: achi4, desc: 'Awarded with excellence in Social Welfare in Education by Education Minister (Rajasthan) Shri B.D. Kala  ' },
 ];
 export const GalleryDataCelebration = [
-  
-  { src: celeb2 , desc: 'Competishun Topper Students in JEE Advanced 2023 celebrated their success by creating CS Logo at JAIGHOSH 2023' },
-  { src: celeb7 , desc: 'Inaugurating JAIGHOSH 2023 cerimony by MT sir, ABJ sir , ALK sir , NS sir' },
-  { src:celeb11  , desc: 'Group photograph of Competishun students selected in JEE ADVANCED 2023' },
-  { src:celeb13  , desc: "Teacher's Day celebration at Competishun Online Campus" },
-  { src:celeb17  , desc: 'राम मंदिर प्राण प्रतिष्ठा Celebration at Competishun Offline Campus' },
-  { src:celeb23  , desc: 'Foundation Day (23 May) celebration at SHAKUN HOTEL' },
+
+  { src: celeb2, desc: 'Competishun Topper Students in JEE Advanced 2023 celebrated their success by creating CS Logo at JAIGHOSH 2023' },
+  { src: celeb7, desc: 'Inaugurating JAIGHOSH 2023 cerimony by MT sir, ABJ sir , ALK sir , NS sir' },
+  { src: celeb11, desc: 'Group photograph of Competishun students selected in JEE ADVANCED 2023' },
+  { src: celeb13, desc: "Teacher's Day celebration at Competishun Online Campus" },
+  { src: celeb17, desc: 'राम मंदिर प्राण प्रतिष्ठा Celebration at Competishun Offline Campus' },
+  { src: celeb23, desc: 'Foundation Day (23 May) celebration at SHAKUN HOTEL' },
 ];
